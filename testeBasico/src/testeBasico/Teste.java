19bb0d86db9a1d59cba033a8e25dd6dc876c3b0d
AlvesThales <thalesguimaraes@gmail.com>
2019-08-27 16:44:28 -0300

    array examples

---
+++ testeBasico/src/testeBasico/Teste.java
@@ -0,0 +1,15 @@
+package testeBasico;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		String nome = "Thales";
+		
+		nome = "Alves";
+		
+		System.out.println(nome);
+		
+	}
+
+}
